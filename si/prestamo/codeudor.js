1c497d24dafcb8c57e46b61018c794322728263e
Jeremy Reyes B <jjrb6@hotmail.com>
2018-01-15 20:06:12 -0500

    Consultar codeudor.

--- si/prestamo/codeudor.js
+++ si/prestamo/codeudor.js
@@ -1,215 +1,327 @@
-/**
- * Created by Jose Barrios on 2/09/2017.
- */
 
-/* Nueva version */
 Api.Codeudor = {
+    id: null,
+    uri: null,
     carpeta: 'Prestamo',
     controlador: 'Codeudor',
-    ajaxSimple: Api.Ajax.ajaxSimple,
-    uri: null,
+    nombreTabla: 'codeudor-tabla',
+    idModal: '#modal-codeudor ',
+    idCliente: null,
 
-    consultarPorIdCliente: null,
-    cpId: null,
-    guardar: null,
-    actualizarRegistro: null,
-    eliminarRegistro: null,
+    $ajaxC: Api.Ajax.constructor,
+    $ajaxT: Api.Ajax.ajaxTabla,
+    $ajaxS: Api.Ajax.ajaxSimple,
+    $mensajeS: Api.Mensaje.superior,
+    $uriCrud: Api.Uri.crudObjecto,
+    $funcionalidadesT: Api.Elementos.funcionalidadesTabla(),
 
-    idRetorno: null,
-    json: null,
-    jsonCodeudor: null,
+    _InicializarFormulario: null,
+    _ConsultarPorClientePaginado: null,
+    _CrearActualizar: null,
+    _CambiarEstado: null,
+    _Eliminar: null,
 
-    constructor: function(idRetorno) {
-        this.consultarPorIdCliente  = this.uriCrud('ConsultarPorIdCliente',this.controlador,this.carpeta)+'&';
-        this.cpId                   = this.uriCrud('ConsultarPorId',this.controlador,this.carpeta)+'&';
-        this.guardar                = this.uriCrud('Guardar',this.controlador,this.carpeta)+'&';
-        this.actualizarRegistro     = this.uriCrud('Actualizar',this.controlador,this.carpeta)+'&';
-        this.eliminarRegistro       = this.uriCrud('Eliminar',this.controlador,this.carpeta)+'&';
-        str                         = this.controlador;
-        this.uri                    = str.toLowerCase();
-        this.idRetorno              = idRetorno;
+    constructor: function(id, json) {
+        this._InicializarFormulario	        = this.$uriCrud('InicializarFormulario',this.controlador,this.carpeta);
+        this._ConsultarPorClientePaginado	= this.$uriCrud('ConsultarPorClientePaginado',this.controlador,this.carpeta);
+        this._CrearActualizar	            = this.$uriCrud('CrearActualizar',this.controlador,this.carpeta);
+        this._CambiarEstado                 = this.$uriCrud('CambiarEstado',this.controlador,this.carpeta);
+        this._Eliminar                      = this.$uriCrud('Eliminar',this.controlador,this.carpeta);
 
-        $('#'+idRetorno).html('');
+        str         	= this.controlador;
+        this.uri    	= str.toLowerCase();
+        this.idCliente  = id;
 
-        return true;
+        this.tabla();
+        /*this.inicializarFormulario();*/
+
+        $(this.idModal + '#nombre-completo').text(json.nombres + ' ' + json.apellidos);
+        $(this.idModal).modal('show');
+
     },
 
-    consultarPorCliente: function(id) {
+    tabla: function(pagina,tamanhio) {
 
-        this.constructor('modal-detalle #mensaje')
+        this.$ajaxC(this.nombreTabla,pagina,tamanhio);
 
-        this.ajaxSimple(
-            this.idRetorno,
+        this._ConsultarPorClientePaginado['id_cliente'] = this.idCliente;
+
+        this.$ajaxT(
+            this.nombreTabla,
             this.uri,
-            this.consultarPorIdCliente + '&id='+id,
-
-            function(json){
-
-                Api.Codeudor.json = json;
+            this._ConsultarPorClientePaginado,
+            {
+                objecto: this.controlador,
+                metodo: 'tabla',
+                funcionalidades: this.$funcionalidadesT,
+                opciones: this.opciones(),
+                checkbox: false,
+                columnas: [
+                    {nombre: 'cedula',              edicion: false,	formato: 'numerico', alineacion:'centrado'},
+                    //{nombre: 'fecha_expedicion',    edicion: false,	formato: '', alineacion:'centrado'},
+                    {nombre: 'nombres',             edicion: false,	formato: '', alineacion:'izquierda'},
+                    {nombre: 'apellidos',           edicion: false,	formato: '', alineacion:'izquierda'},
+                    {nombre: 'direccion',           edicion: false,	formato: '', alineacion:'izquierda'},
+                    {nombre: 'telefono',            edicion: false,	formato: '', alineacion:'centrado'},
+                    {nombre: 'celular',             edicion: false,	formato: '', alineacion:'centrado'}
+                ],
+                automatico: false
             }
-        )
+        );
     },
 
-    consultarPorId: function(id) {
+    crearActualizar: function() {
 
-        this.ajaxSimple(
-            this.idRetorno,
-            this.uri,
-            this.cpId + '&id='+id,
+        var $objeto = Api[this.controlador];
+        var parametros = this.verificarFormulario(this._CrearActualizar);
 
-            function(json){
-
-                Api.Codeudor.jsonCodeudor = json;
-            }
-        )
-    },
-
-    agregar: function(idFormulario,idMensaje) {
-
-        this.constructor(idMensaje)
-
-        if(this.verificarFormulario(idFormulario,idMensaje)) {
-
-            this.ajaxSimple(
-                this.idRetorno,
+        if (parametros) {
+            this.$ajaxS(
+                '',
                 this.uri,
-                this.guardar + '&' + $(idFormulario).serialize(),
+                parametros,
 
-                function(json){
+                function (json) {
 
-                    var $codeudor = Api.Codeudor;
+                    Api.Mensaje.jsonSuperior(json);
 
-                    $codeudor.messageResultJson(json,'mensaje-crud');
+                    if (json.resultado === 1) {
 
-                    if (json.resultado == 1) {
+                        var AH = Api.Herramientas;
 
-                        console.log(idFormulario.substring(1).trim());
-                        document.getElementById(idFormulario.substring(1).trim()).reset();
-                        $codeudor.consultarPorCliente($('#id_cliente').val());
+                        $objeto.id = null;
+                        $objeto.constructor();
+
+                        AH.cancelarCA('cliente');
 
                         setTimeout(function(){
-                            Api.Cliente.listarTablaCodeudor();
+                            AH.cambiarPestanhia('pestanhia-cliente','informacion');
                         }, 1000);
                     }
                 }
-            )
+            );
         }
+
     },
 
-    actualizar: function(id,idFormulario,idMensaje) {
+    editar: function(id,objeto) {
 
-        this.constructor(idMensaje)
+        this.id = id;
 
-        if(this.verificarFormulario(idFormulario,idMensaje)) {
+        var $objeto = Api[this.controlador];
+        var AH      = Api.Herramientas;
 
-            this.ajaxSimple(
-                this.idRetorno,
-                this.uri,
-                this.actualizarRegistro + '&' + $(idFormulario).serialize()
-                                        + '&id=' + id,
+        AH.selectDefault('#id-tipo-identificacion',objeto.id_tipo_identificacion);
+        AH.selectDefault('#id-estado-civil',objeto.id_estado_civil);
+        AH.selectDefault('#id-ocupacion',objeto.id_ocupacion);
+        AH.selectDefault('#id-banco-cliente',objeto.id_banco_cliente);
 
-                function(json){
+        $('#identificacion').val(objeto.identificacion);
+        $('#nombres').val(objeto.nombres);
+        $('#apellidos').val(objeto.apellidos);
+        $('#fecha-nacimiento').val(objeto.fecha_nacimiento);
+        $('#email-personal').val(objeto.email_personal);
+        $('#id-municipio').val(objeto.id_municipio);
+        $('#ciudad').val(objeto.ciudad);
+        $('#direccion').val(objeto.direccion);
+        $('#barrio').val(objeto.barrio);
+        $('#telefono').val(objeto.telefono);
+        $('#celular').val(objeto.cellIndex);
+        $('#empresa-nombre').val(objeto.empresa_nombre);
+        $('#empresa-cargo').val(objeto.empresa_cargo);
+        $('#empresa-area').val(objeto.empresa_area);
+        $('#empresa-barrio').val(objeto.empresa_barrio);
+        $('#empresa-direccion').val(objeto.empresa_direccion);
+        $('#empresa-telefono').val(objeto.empresa_telefono);
+        $('#empresa-fecha-ingreso').val(objeto.empresa_fecha_ingreso);
+        $('#empresa-antiguedad-meses').val(objeto.empresa_antiguedad_meses);
+        $('#no-cuenta').val(objeto.no_cuenta);
+        $('#sueldo').val(objeto.sueldo);
+        $('#ingresos').val(objeto.ingresos);
+        $('#egresos').val(objeto.egresos);
+        $('#ref-personal-nombres').val(objeto.ref_personal_nombres);
+        $('#ref-personal-apellidos').val(objeto.ref_personal_apellidos);
+        $('#ref-personal-barrio').val(objeto.ref_personal_barrio);
+        $('#ref-personal-telefono').val(objeto.ref_personal_telefono);
+        $('#ref-personal-celular').val(objeto.ref_personal_celular);
+        $('#ref-familiar-nombres').val(objeto.ref_familiar_nombres);
+        $('#ref-familiar-apellidos').val(objeto.ref_familiar_apellidos);
+        $('#ref-familiar-barrio').val(objeto.ref_familiar_barrio);
+        $('#ref-familiar-telefono').val(objeto.ref_familiar_telefono);
+        $('#ref-familiar-celular').val(objeto.ref_familiar_celular);
+        $('#observaciones').val(objeto.observaciones);
 
-                    var $codeudor = Api.Codeudor;
-
-                    $codeudor.messageResultJson(json,'mensaje-crud');
-
-                    if (json.resultado == 1) {
-
-                        $codeudor.consultarPorCliente($('#id_cliente').val());
-                        Api.Cliente.cancelarEditarCodeudor('#form-codeudores');
-
-                        setTimeout(function(){
-                            Api.Cliente.listarTablaCodeudor();
-                        }, 1000);
-                    }
-                }
-            )
-        }
+        AH.mostrarBotonesActualizar('cliente');
+        AH.cambiarPestanhia($objeto.idContenedor + ' #pestanhia-cliente','informacion');
     },
 
-    confirmarEliminar: function(id) {
+    cambiarEstado: function(id) {
 
-        $('#modal-eliminar #siModalEliminar').attr('onClick','Api.Codeudor.eliminar('+id+')');
-        $('#modal-eliminar').modal('show');
+        var $objeto = Api[this.controlador];
+
+        this._CambiarEstado['id'] = id;
+
+        this.$ajaxS(
+            '',
+            this.uri,
+            this._CambiarEstado,
+
+            function () {
+
+                $objeto.tabla();
+            }
+        );
     },
 
     eliminar: function(id) {
 
-        this.ajaxSimple(
-            this.idRetorno,
-            this.uri,
-            this.eliminarRegistro + '&id=' + id,
+        var $objeto = Api[this.controlador];
 
-            function(json){
+        this._Eliminar['id'] = id;
 
-                var $codeudor = Api.Codeudor;
+        swal({
+            title: "¿Seguro que desea eliminarlo?",
+            text: "Después de eliminarlo no podrás recuperar esta información ni revertir los cambios!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Sí, deseo eliminarlo",
+            cancelButtonText: "Cancelar",
+            closeOnConfirm: false
+        }, function () {
 
-                $codeudor.messageResultJson(json,'mensaje-crud');
+            $objeto.$ajaxS(
+                '',
+                $objeto.uri,
+                $objeto._Eliminar,
 
-                if (json.resultado == 1) {
+                function (json) {
 
-                    $codeudor.consultarPorCliente($('#id_cliente').val());
+                    if (json.resultado === 1) {
 
-                    setTimeout(function(){
-                        Api.Cliente.listarTablaCodeudor();
-                    }, 1000);
+                        swal("Eliminado!", json.mensaje, "success");
+                        $objeto.constructor();
+                    }
+                    else {
+                        swal("Error", json.mensaje , "error");
+                    }
                 }
+            );
+        });
+    },
+
+    verificarFormulario: function($objeto) {
+
+        $objeto['id_tipo_identificacion']   = $('#id-tipo-identificacion').val();
+        $objeto['identificacion']           = $('#identificacion').val().trim();
+        $objeto['nombres']                  = $('#nombres').val().trim();
+        $objeto['apellidos']                = $('#apellidos').val().trim();
+        $objeto['id_estado_civil']          = $('#id-estado-civil').val().trim();
+        $objeto['fecha_nacimiento']         = $('#fecha-nacimiento').val().trim();
+        $objeto['email_personal']           = $('#email-personal').val().trim();
+        $objeto['id_municipio']             = $('#id-municipio').val().trim();
+        $objeto['direccion']                = $('#direccion').val().trim();
+        $objeto['barrio']                   = $('#barrio').val().trim();
+        $objeto['telefono']                 = $('#telefono').val().trim();
+        $objeto['celular']                  = $('#celular').val().trim();
+        $objeto['id_ocupacion']             = $('#id-ocupacion').val();
+        $objeto['empresa_nombre']           = $('#empresa-nombre').val().trim();
+        $objeto['empresa_cargo']            = $('#empresa-cargo').val().trim();
+        $objeto['empresa_area']             = $('#empresa-area').val().trim();
+        $objeto['empresa_barrio']           = $('#empresa-barrio').val().trim();
+        $objeto['empresa_direccion']        = $('#empresa-direccion').val().trim();
+        $objeto['empresa_telefono']         = $('#empresa-telefono').val().trim();
+        $objeto['empresa_fecha_ingreso']    = $('#empresa-fecha-ingreso').val().trim();
+        $objeto['empresa_antiguedad_meses'] = $('#empresa-antiguedad-meses').val().trim();
+        $objeto['id_banco_cliente']         = $('#id-banco-cliente').val();
+        $objeto['no_cuenta']                = $('#no-cuenta').val().trim();
+        $objeto['sueldo']                   = $('#sueldo').val().trim();
+        $objeto['ingresos']                 = $('#ingresos').val().trim();
+        $objeto['egresos']                  = $('#egresos').val().trim();
+        $objeto['ref_personal_nombres']     = $('#ref-personal-nombres').val().trim();
+        $objeto['ref_personal_apellidos']   = $('#ref-personal-apellidos').val().trim();
+        $objeto['ref_personal_barrio']      = $('#ref-personal-barrio').val().trim();
+        $objeto['ref_personal_telefono']    = $('#ref-personal-telefono').val().trim();
+        $objeto['ref_personal_celular']     = $('#ref-personal-celular').val().trim();
+        $objeto['ref_familiar_nombres']     = $('#ref-familiar-nombres').val().trim();
+        $objeto['ref_familiar_apellidos']   = $('#ref-familiar-apellidos').val().trim();
+        $objeto['ref_familiar_barrio']      = $('#ref-familiar-barrio').val().trim();
+        $objeto['ref_familiar_telefono']    = $('#ref-familiar-telefono').val().trim();
+        $objeto['ref_familiar_celular']     = $('#ref-familiar-celular').val().trim();
+        $objeto['observaciones']            = $('#observaciones').val().trim();
+
+        $objeto['id'] = this.id;
+
+
+        if (!$objeto['id_tipo_identificacion']) {
+            this.$mensajeS('advertencia','Advertencia','Debe seleccionar un tipo de identificación para continuar');
+            return false;
+        }
+
+        if (!$objeto['identificacion']) {
+            this.$mensajeS('advertencia','Advertencia','Debe digitar la identificación para continuar');
+            return false;
+        }
+
+        if (!$objeto['nombres']) {
+            this.$mensajeS('advertencia','Advertencia','Debe digitar los nombres para continuar');
+            return false;
+        }
+
+        if (!$objeto['apellidos']) {
+            this.$mensajeS('advertencia','Advertencia','Debe digitar los apellidos para continuar');
+            return false;
+        }
+
+        if (!$objeto['direccion']) {
+            this.$mensajeS('advertencia','Advertencia','Debe digitar la dirección para continuar');
+            return false;
+        }
+
+        return $objeto;
+    },
+
+    opciones: function() {
+        return {
+            parametrizacion: [
+                {
+                    nombre: 'Actualizar',
+                    icono: 'fa-pencil-square-o',
+                    accion: 'Api.' + this.controlador + '.editar',
+                    color: '#1a7bb9',
+                    estado: false,
+                    permiso: 'actualizar',
+                    informacion: true
+                },
+                {
+                    nombre: 'Eliminar',
+                    icono: 'fa-trash',
+                    accion: 'Api.' + this.controlador + '.eliminar',
+                    color: '#ec4758',
+                    estado: false,
+                    permiso: 'eliminar',
+                    informacion: false
+                }
+            ]
+        };
+    },
+
+    inicializarFormulario: function() {
+
+        this.$ajaxS(
+            '',
+            this.uri,
+            this._InicializarFormulario,
+
+            function (json) {
+
+                var AH = Api.Herramientas;
+
+                AH.cargarSelectJSON('#id-tipo-identificacion',json.tipo_identificacion,true);
+                AH.cargarSelectJSON('#id-estado-civil',json.estado_civil,true);
+                AH.cargarSelectJSON('#id-ocupacion',json.ocupacion,true);
+                AH.cargarSelectJSON('#id-banco-cliente',json.bancos,true);
             }
-        )
-    },
-
-    verificarFormulario: function(idFormulario, idMensaje) {
-
-        if (!$(idFormulario+'#cedula').val()) {
-            _mensaje('advertencia',idMensaje,'Digite el campo cedula para continuar')
-            return false;
-        }
-
-        if (!$(idFormulario+'#fecha_expedicion').val()) {
-            _mensaje('advertencia',idMensaje,'Digite seleccione la fecha de expedición para continuar')
-            return false;
-        }
-
-        if (!$(idFormulario+'#nombres').val()) {
-            _mensaje('advertencia',idMensaje,'Digite el campo nombres para continuar')
-            return false;
-        }
-
-        if (!$(idFormulario+'#apellidos').val()) {
-            _mensaje('advertencia',idMensaje,'Digite el campo apellidos para continuar')
-            return false;
-        }
-
-        if (!$(idFormulario+'#direccion').val()) {
-            _mensaje('advertencia',idMensaje,'Digite el campo dirección para continuar')
-            return false;
-        }
-
-        return true;
-    },
-
-    uriCrud: function(accion,controlador,carpeta) {
-
-        return 'crud=true&padre='+$('#idPadre').val()
-            +'&hijo='+$('#idHijo').val()
-            +'&funcionesVariables='+accion
-            +'&controlador='+controlador
-            +'&carpetaControlador='+carpeta;
-    },
-
-    messageResultJson: function(json,id) {
-
-        switch (json.resultado) {
-            case 1:
-                _mensaje('realizado',id, json.mensaje);
-                break;
-            case 0:
-                _mensaje('advertencia',id, json.mensaje);
-                break;
-            case -1:
-                _mensaje('error',id, json.mensaje);
-                break;
-        }
-    },
-}
+        );
+    }
+};
